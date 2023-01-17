949690f7fda23214a2ae41815197b78c189bb911
Antonio Ossa Guerra <aaossa@uc.cl>
2023-01-17 18:21:42 -0300

    :sparkles: Use trainer in main script
    This change reduces and simplifies the code in the main script
    
    Signed-off-by: Antonio Ossa Guerra <aaossa@uc.cl>

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ from typing import Dict, List
 import numpy as np
 import torch
 from torch import nn, optim
-from torch.utils.data import DataLoader
-from tqdm.autonotebook import tqdm
 
-from vbpr import VBPR
-from vbpr.datasets import TradesyDataset, TradesySample
+from vbpr import VBPR, Trainer
+from vbpr.datasets import TradesyDataset
 
 RANDOM_SEED = 0
 N_EPOCHS = 50
@@ -64,101 +62,5 @@ if __name__ == "__main__":
         lr=0.001,
     )
 
-    training_dataset: TradesyDataset
-    validation_dataset: TradesyDataset
-    evaluation_dataset: TradesyDataset
-    training_dataset, validation_dataset, evaluation_dataset = dataset.split()
-    training_dataloader: DataLoader[TradesySample] = DataLoader(
-        training_dataset,
-        batch_size=100,
-        shuffle=True,
-        num_workers=num_workers,
-    )
-    validation_dataloader: DataLoader[TradesySample] = DataLoader(
-        validation_dataset,
-        batch_size=100,
-        shuffle=False,
-        num_workers=0,
-    )
-    dataloaders = {
-        "train": training_dataloader,
-        "valid": validation_dataloader,
-    }
-
-    for epoch in range(1, N_EPOCHS + 1):
-        for phase, dataloader in dataloaders.items():
-            # Set correct model mode
-            if phase == "train":
-                model = model.train()
-            else:
-                model = model.eval()
-
-            # Tensor to accumulate results
-            running_acc = torch.tensor(0, dtype=torch.int, device=device)
-            running_loss = torch.tensor(0.0, dtype=torch.double, device=device)
-
-            for uid, iid, jid in tqdm(
-                dataloader, desc=f"Phase: {phase} (epoch={epoch})"
-            ):
-                # Prepare inputs
-                uid = uid.to(device).squeeze()
-                iid = iid.to(device).squeeze()
-                jid = jid.to(device).squeeze()
-                # Clear gradients
-                optimizer.zero_grad()
-                # Forward step
-                with torch.set_grad_enabled(phase == "train"):
-                    outputs = model(uid, iid, jid)
-                    outputs = torch.reshape(outputs, (1, -1))
-                    loss = -torch.nn.functional.logsigmoid(outputs).sum()
-                    if phase == "train":
-                        # Backward
-                        loss.backward()
-                        # Update parameters
-                        optimizer.step()
-
-                # Accumulate batch results
-                running_acc.add_((outputs > 0).sum())
-                running_loss.add_(loss.detach() * outputs.size(0))
-
-            # Display epoch results
-            dataset_size: int = len(dataloader.dataset)  # type: ignore
-            epoch_acc = running_acc.item() / dataset_size
-            epoch_loss = running_loss.item() / dataset_size
-            print(f"Epoch {epoch}/{phase}: ACC={epoch_acc:.6f} LOSS={epoch_loss:.6f}")
-
-        model.eval()
-        cache = model.generate_cache()
-        AUC_valid = torch.zeros(dataset.n_users, device=device)
-        AUC_eval = torch.zeros(dataset.n_users, device=device)
-        for user in tqdm(range(dataset.n_users), desc="AUC on All Items"):
-            items_train = training_dataset.get_user_items(user)
-            items_valid = validation_dataset.get_user_items(user)
-            items_eval = evaluation_dataset.get_user_items(user)
-            user_tensor = torch.tensor([user], device=device)
-
-            x_u_valid = model.recommend(
-                user_tensor, torch.tensor(items_valid, device=device)
-            ).item()
-            x_u_eval = model.recommend(
-                user_tensor, torch.tensor(items_eval, device=device)
-            ).item()
-
-            user_recommendations = model.recommend_all(user_tensor, cache=cache)
-            left_out_items = np.concatenate([items_train, items_valid, items_eval])
-            max_possible = dataset.n_items - left_out_items.shape[0]
-            seen_recommendations = user_recommendations[left_out_items]
-            count_valid = (
-                (x_u_valid > user_recommendations).sum()
-                - (x_u_valid > seen_recommendations).sum()
-            ).item()
-            count_eval = (
-                (x_u_eval > user_recommendations).sum()
-                - (x_u_eval > seen_recommendations).sum()
-            ).item()
-
-            AUC_valid[user] = 1.0 * count_valid / max_possible
-            AUC_eval[user] = 1.0 * count_eval / max_possible
-
-        print(f"AUC_valid={AUC_valid.mean():.6f} | AUC_eval={AUC_eval.mean():.6f}")
-        print()
+    trainer = Trainer(model, optimizer, device=device)
+    trainer.fit(dataset, n_epochs=100)
